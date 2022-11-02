bc6107621228d0f54cc8464b369af24cca95636c
linchuanke <310504719@qq.com>
2022-11-02 15:51:30 +0800

    day7

--- 12_条件类型与infer.ts
+++ 12_条件类型与infer.ts
@@ -109,5 +109,61 @@ type PromiseValueResult4 = PromiseValue2<Promise<Promise<boolean>>>; // boolean
 // 优化，使用递归提取
 type PromiseValue3<T> = T extends Promise<infer V> ? PromiseValue3<V> : T;
 
+// 分布式条件类型
+
+type Condition<T> = T extends 1 | 2 | 3 ? T : never;
+type Res1 = Condition<1 | 2 | 3 | 4 | 5> // 1 | 2 | 3
+type Res2 = 1 | 2 | 3 | 4 | 5 extends 1 | 2 | 3 ? 1 | 2 | 3 | 4 | 5 : never // never;
+
+type Naked<T> = T extends boolean ? "Y" : "N";
+type Wrapped<T> = [T] extends [boolean] ? "Y" : "N";
+
+type Res3 = Naked<number | boolean> // "N" | "Y";
+type Res4 = Wrapped<number | boolean> // "N";
+
+// 对于属于裸类型参数的检查类型，条件类型会在实例化时期自动分发到联合类型上。
+
+// never 与 any完全不同
+
+// 直接使用，返回联合类型
+type Tmp1 = any extends string ? 1 : 2; // 1 | 2;
+type Tmp2<T> = T extends string ? 1 : 2;
+type Tmp2Res = Tmp2<any>; // 1 | 2
+
+// 如果判断条件是any，仍会进行推断
+type Special1 = any extends any ? 1 : 2 // 1
+type Special2<T> = T extends any ? 1 : 2;
+type Special2Res = Special2<any>; // 1
+
+// 直接使用，仍然会进行判断
+type Tmp3 = never extends string ? 1 : 2; // 1
+
+type Tmp4<T> = T extends string ? 1 : 2;
+// 通过泛型参数传入，会跳过判断
+type Tmp4Res = Tmp4<never>; // never
+
+// 如果判断条件是 never，还是仅在作为泛型参数时才跳过判断
+type Special3 = never extends never ? 1 : 2; // 1
+type Special4<T> = T extends never ? 1 : 2;
+type Special4Res = Special4<never>; // never
+
+// IsAny
+type IsAny<T> = 0 extends 1 & T ? true : false;
+type IsAnyRes = IsAny<any>;
+
+// IsNever
+type IsNever<T> = [T] extends [never] ? true : false
+
+// IsUnknown
+
+type IsUnknown<T> = IsNever<T> extends false
+  ? T extends unknown
+  ? unknown extends T
+  ? IsAny<T> extends false
+  ? true
+  : false
+  : false
+  : false
+  : false;
 
 export { }
