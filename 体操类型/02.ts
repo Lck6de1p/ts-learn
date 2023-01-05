0f4fa8f6de56930c999f89ea8d654b7faa5f3816
linchuanke <310504719@qq.com>
2023-01-05 10:48:03 +0800

    feat: 重新构造做变换

--- 体操类型/02.ts
+++ 体操类型/02.ts
@@ -91,6 +91,16 @@ type ToRequired<T> = {
 };
 type ToRequiredRes = ToRequired<obj>;
 
+/**
+ * 
+ * 
+ *  类型参数 Obj 为要处理的索引类型，通过 extends 约束为索引为 string，值为任意类型的索引类型 Record<string, any>。
+    类型参数 ValueType 为要过滤出的值的类型。
+    构造新的索引类型，索引为 Obj 的索引，也就是 Key in keyof Obj，但要做一些变换，也就是 as 之后的部分。
+    如果原来索引的值 Obj[Key] 是 ValueType 类型，索引依然为之前的索引 Key，否则索引设置为 never，never 的索引会在生成新的索引类型时被去掉。
+    值保持不变，依然为原来索引的值，也就是 Obj[Key]。
+    这样就达到了过滤索引类型的索引，产生新的索引类型的目的
+ */
 type FilterByValueType<Obj extends Record<string, any>, ValueType> = {
   [Key in keyof Obj as Obj[Key] extends ValueType ? Key : never]: Obj[Key];
 };
